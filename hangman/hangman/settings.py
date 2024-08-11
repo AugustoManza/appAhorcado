5fef3db47fe96ba7f0431039485cff123d836cf4
Francisco <franciscomanzanarez1996@gmail.com>
2024-08-11 15:59:23 -0600

    2do commit

--- hangman/hangman/settings.py
+++ hangman/hangman/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'hangmanbd',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
